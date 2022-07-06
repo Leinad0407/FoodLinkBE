0a67849f6d95f4c9ace857e8a8e9104896693c53
Daniel Aguilar <ing.aguilardan@gmail.com>
2022-07-06 05:01:47 -0500

    Primer maquetado de pagina de beneficiarios

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import logo from "./logo.svg";
 import "./App.scss";
 import "./sass/styles.scss";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import "./App.scss";
+
+//Components
+import Donadores from "./pages/Donadores";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<p>Estoy en el dashboard</p>} />
-        <Route path="about" element={<p>Estoy en about</p>} />
+        <Route path="/donadores" element={<Donadores />} />
       </Routes>
     </div>
   );
