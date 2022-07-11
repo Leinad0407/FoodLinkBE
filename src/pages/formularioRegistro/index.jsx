87eaa6064d6ff6dfb5e23c4ed39ead851ac09610
Aaron21King <lovespurs1882@gmail.com>
2022-07-11 09:12:29 -0600

    editando formulario-registro

--- src/pages/formularioRegistro/index.jsx
+++ src/pages/formularioRegistro/index.jsx
@@ -2,7 +2,7 @@ import "../formularioRegistro/formularioRegistro.scss";
 
 export default function Registro(){
     return(
-        <div className="container">
+        <div className="container ">
             <section class="d-flex justify-content-center align-items-center">
             <div class="card shadow col-xs-12 col-sm-6 col-md-6 col-lg-4   p-4">
                 <div class="mb-4 d-flex justify-content-start align-items-center">
