ec2cc25e871c22379b9c2606afb05ec940d72535
Daniel Aguilar <ing.aguilardan@gmail.com>
2022-07-31 15:45:23 -0500

    adding route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.scss";
 import "./sass/styles.scss";
 import { Routes, Route, Link } from "react-router-dom";
@@ -6,25 +6,36 @@ import "./App.scss";
 
 //Components
 import Donadores from "./pages/Donadores";
-// import Home from "./pages/Home";
-// import Registro from "./pages/formularioRegistro";
+import Home from "./pages/Home";
+import Registro from "./pages/formularioRegistro";
+import RegistroDonador from "./pages/formularioRegistroDonador";
 import CreateDonations from "./pages/CreateDonations";
-// import DonationDetail from "./pages/Detail/Detail";
-import Donors from "./pages/Donors";
+import DonationDetail from "./pages/Detail/Detail";
+import Profile from "./pages/profile/profile";
+import ProfileEdit from "./pages/profileEdit/profileEdit";
+import UserDonations from "./pages/UserDonations/userDonations";
+import OrderReady from "./pages/OrderReady/orderReady";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        {/* <Route path="donations/detail/:id" element={<DonationDetail />} /> */}
-
+        <Route path="donations/detail/:id" element={<DonationDetail />} />
         <Route path="/donadores" element={<Donadores />} />
-      </Routes>
-      <Routes>
-        {/* <Route path="/" element={<Home />} /> */}
-        {/* <Route path="/registro" element={<Registro />} /> */}
-      </Routes>
-      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="donations" element={<Donadores />} />
+        <Route path="donations/detail/:id" element={<DonationDetail />} />
+        <Route
+          path="donations/detail/:id/OrderReady"
+          element={<OrderReady />}
+        />
+        <Route path="/" element={<Donadores />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/profile/edit" element={<ProfileEdit />} />
+        <Route path="/profile/donations" element={<UserDonations />} />
+        <Route path="/registro" element={<Registro />} />
+        <Route path="/registroDonador" element={<RegistroDonador />} />
+
         <Route path="/createDonations" element={<CreateDonations />} />
       </Routes>
       <Routes>
