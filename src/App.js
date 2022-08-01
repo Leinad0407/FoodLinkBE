aeb11c308233963c2cef10635034eff2ebbdd7f9
Daniel Aguilar <ing.aguilardan@gmail.com>
2022-07-31 21:31:00 -0500

    adding donation page with route

--- src/App.js
+++ src/App.js
@@ -8,22 +8,13 @@ import "./App.scss";
 import Donadores from "./pages/Donadores";
 import Home from "./pages/Home";
 import Registro from "./pages/formularioRegistro";
+import CreateDonations from "./pages/CreateDonations";
 import RegistroDonador from "./pages/formularioRegistroDonador";
-import CreateDonations from "./pages/CreateDonations";
-import Home from "./pages/Home";
-import CreateDonations from "./pages/CreateDonations";
-import Registro from "./pages/formularioRegistro";
-import RegistroDonador from "./pages/formularioRegistroDonador";
-import CreateDonations from "./pages/CreateDonations";
 import DonationDetail from "./pages/Detail/Detail";
 import Profile from "./pages/profile/profile";
 import ProfileEdit from "./pages/profileEdit/profileEdit";
 import UserDonations from "./pages/UserDonations/userDonations";
 import OrderReady from "./pages/OrderReady/orderReady";
-
-// import DonationDetail from "./pages/Detail/Detail";
-
-import Donors from "./pages/Donors";
 
 function App() {
   return (
@@ -45,7 +36,6 @@ function App() {
         <Route path="/registro" element={<Registro />} />
         <Route path="/registroDonador" element={<RegistroDonador />} />
         <Route path="/createDonations" element={<CreateDonations />} />
-        <Route path="/Donors" element={<Donors />} />
       </Routes>
     </div>
   );
--- src/pages/Donors/index.jsx
+++
@@ -1,5 +0,0 @@
-import "../Donors/Donors.scss";
-
-export default function Donors() {
-  return <h1>Esta es la página de Donadores</h1>;
-}
