b4bf670d4e1149c9d779b7fe383aab56db860ef5
Alan Guerrero García <age_allison@hotmail.com>
2022-08-06 18:34:31 -0500

    Edición de estilo detalle de perfil

--- src/pages/profile/profile.jsx
+++ src/pages/profile/profile.jsx
@@ -7,68 +7,66 @@ import { BsArrowLeft } from "react-icons/bs";
 //Import de componentes necesarios
 import Footer from "../../components/footer/footer";
 
-//import de estilos//
-import "./profile.scss";
+// //import de estilos//
+// import "./profile.scss";
 
 export default function Profile() {
   const navigate = useNavigate();
   return (
     <div>
       <div className="row">
-        <Header
-          className="col col-lg-12 col-12"
-          ph1="Detalle de"
-          ph2="  tu perfil"
-        />
-        <Button
-          id="backBtn"
-          className="col col-lg-2 selectButton"
-          variant="success"
-          onClick={() => navigate(-1)}
-        >
-          <BsArrowLeft size={25} />
-        </Button>
+        <Header className="" ph1="Detalle de" ph2="  tu perfil" />
 
-        <div className="container">
-          <div className="row">
-            <img></img>
-            <TitAndSub
-              title="Nombre de usuario:"
-              info="Aqui debe ir el nombre"
-            />
-            <TitAndSub
-              title="Correo electrónico:"
-              info="Aqui debe ir el correo"
-            />
-            <TitAndSub
-              title="Número de contacto:"
-              info="Aqui debe ir el numero"
-            />
-            <TitAndSub
-              title="Dirección de recolección:"
-              info="Aqui debe ir el address"
-            />
-            <div id="profileButtons">
-              <Button
-                className="col col-lg-2 selectButton"
-                onClick={() => navigate("edit")}
-                variant="success"
-              >
-                {" "}
-                Editar mis datos{" "}
-              </Button>
-              {/* Este botón nos dirige a la pantalla donde podremos ver las donaciones
+        <div id="profileInfo" className="container">
+          <Button
+            // id="backBtn"
+            className="col col-lg-2 selectButton"
+            variant="success"
+            onClick={() => navigate(-1)}
+          >
+            <BsArrowLeft size={25} />
+          </Button>
+
+          <img alt=""></img>
+          <TitAndSub
+            className="col col-lg-12"
+            title="Nombre de usuario:"
+            info="Aqui debe ir el nombre"
+          />
+          <TitAndSub
+            className="col col-lg-12"
+            title="Correo electrónico:"
+            info="Aqui debe ir el correo"
+          />
+          <TitAndSub
+            className="col col-lg-12"
+            title="Número de contacto:"
+            info="Aqui debe ir el numero"
+          />
+          <TitAndSub
+            className="col col-lg-12"
+            title="Dirección de recolección:"
+            info="Aqui debe ir el address"
+          />
+
+          <Button
+            className="col col-lg-2 selectButton"
+            onClick={() => navigate("edit")}
+            variant="success"
+          >
+            {" "}
+            Editar mis datos{" "}
+          </Button>
+          {/* Este botón nos dirige a la pantalla donde podremos ver las donaciones
         que ha realizado este usuario */}
-              <Button
-                onClick={() => navigate("donations")}
-                className=" col col-lg-2 selectButton"
-                variant="success"
-              >
-                {" "}
-                Mis Donaciones{" "}
-              </Button>
-            </div>
-          </div>
+          <Button
+            onClick={() => navigate("donations")}
+            className=" col col-lg-2 selectButton"
+            variant="success"
+          >
+            {" "}
+            Mis Donaciones{" "}
+          </Button>
         </div>
 
         <Footer />
