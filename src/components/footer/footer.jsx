a485fb06afab37de35909258586c7be004227479
Alan Guerrero García <age_allison@hotmail.com>
2022-08-06 17:15:33 -0500

    Correción de estilos de detalle de comida

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,15 +2,22 @@ import "./footer.scss";
 export default function Footer() {
   return (
     <div id="fatherContainer">
-      <div id="firstPart">
-        <img src="https://pbs.twimg.com/media/FX_2fGBWAAAON7X?format=png&name=240x240"></img>
-        <p className="FoodLinkAddress">
+      <div id="firstPart" className="row">
+        <div id="splitter" className="col col-lg-12"></div>
+        <img
+          className="col col-lg-4 col-12"
+          src="https://pbs.twimg.com/media/FX_2fGBWAAAON7X?format=png&name=240x240"
+        ></img>
+        <div className="col col-lg-4 col-12"></div>
+        <p className=" col col-lg-4 col-12 FoodLinkAddress">
           Puebla 308 Col. Roma Norte
           <br />
           Ciudad de México
         </p>
+        <p className="col col-lg-12 col-12" id="rights">
+          © 2022 FoodLink. Todos los derechos reservados.{" "}
+        </p>
       </div>
-      <p id="rights">© 2022 FoodLink. Todos los derechos reservados. </p>
     </div>
   );
 }
