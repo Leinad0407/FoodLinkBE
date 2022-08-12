ddcc07d2b5524c520e583b1ee406a8720cc58558
Alan Guerrero García <age_allison@hotmail.com>
2022-08-11 19:09:04 -0500

    Edit booked service code

--- src/services/getBookedDonations.js
+++ src/services/getBookedDonations.js
@@ -1,5 +1,5 @@
 export const getBookedDonations = async () => {
-  const response = await fetch(`http://localhost:8080/donations`);
+  const response = await fetch(`http://localhost:8080/profile/bookedDonations`);
   const donations = await response.json();
   return donations;
 };
